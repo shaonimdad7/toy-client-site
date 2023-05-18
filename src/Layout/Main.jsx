4dd6b71a2ea4cbd36b4a72afefe3d33fec9ba0a8
Shaon Imdad <shaonimdad7@gmail.com>
2023-05-18 17:17:28 +0600

    header added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Home from '../Pages/Home/Home/Home';
 import Footer from '../Pages/Shared/Footer/Footer';
+import Navbar from '../Pages/Shared/Footer/Navbar/Navbar';
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -5,29 +5,29 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className=' bg-pink-200'>
-            <footer className="max-w-7xl mx-auto footer p-10 text-base-content">
+            <footer className="max-w-6xl mx-auto footer p-10 text-base-content">
                 <div>
                     <div className='footer_img'>
                         <img src={img} alt="" />
                     </div>
-                    <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+                    <p className='font-bold'>Princess Paradise<br />Providing reliable Toys since 1992</p>
                 </div>
                 <div>
-                    <span className="footer-title">Services</span>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
+                    <span className="footer-title text-2xl">Category</span>
+                    <a className="link link-hover">Barbie Dolls</a>
+                    <a className="link link-hover">Activity cubes</a>
+                    <a className="link link-hover">Shape sorters</a>
+                    <a className="link link-hover">Activity gyms</a>
                 </div>
                 <div>
-                    <span className="footer-title">Company</span>
+                    <span className="footer-title text-2xl">Company</span>
                     <a className="link link-hover">About us</a>
                     <a className="link link-hover">Contact</a>
                     <a className="link link-hover">Jobs</a>
                     <a className="link link-hover">Press kit</a>
                 </div>
                 <div>
-                    <span className="footer-title">Legal</span>
+                    <span className="footer-title text-2xl">Legal</span>
                     <a className="link link-hover">Terms of use</a>
                     <a className="link link-hover">Privacy policy</a>
                     <a className="link link-hover">Cookie policy</a>
