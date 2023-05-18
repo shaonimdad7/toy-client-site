aab62a909ed32d564a517b60175b3b187a16bda9
Shaon Imdad <shaonimdad7@gmail.com>
2023-05-18 15:51:30 +0600

    footer section added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Home from '../Pages/Home/Home/Home';
+import Footer from '../Pages/Shared/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import img from '../../../assets/barbie.png'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className=' bg-pink-200'>
+            <footer className="max-w-7xl mx-auto footer p-10 text-base-content">
+                <div>
+                    <div className='footer_img'>
+                        <img src={img} alt="" />
+                    </div>
+                    <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+                </div>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
