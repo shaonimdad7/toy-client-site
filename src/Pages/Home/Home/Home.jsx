05c83d5e408c95f2baac83bd5f10705d718fe71c
Shaon Imdad <shaonimdad7@gmail.com>
2023-05-20 03:01:36 +0600

    gallary added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
-import Shopby from '../Shopby/Shopby';
+import Gallary from '../Gallary/Gallary';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <Shopby></Shopby>
+            <Gallary></Gallary>
         </div>
     );
 };
